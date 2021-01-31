92a739c06d9775e18a81309429e941cc9d6aa8dd
ihab-sensei <ib-mashhadani@hotmail.com>
2021-01-31 16:40:22 +0300

    Alerts

--- App.js
+++ App.js
@@ -12,25 +12,23 @@ import {
 } from "react-native";
 
 export default function App() {
-  let x = 1;
-  const handleTextPress = () => console.log("Apps is exectued");
-
   return (
     <View style={styles.container}>
-      <Text onPress={handleTextPress} numberOfLines={1}>
-        so close no matter how far, couldn't be much more from the heart.
-        Forever trust in who we are and nothing else matters.
-      </Text>
-      <TouchableHighlight onPress={handleTextPress}>
-        <Image
-          fadeDuration={1000}
-          source={{
-            width: 200,
-            height: 300,
-            uri: "https://picsum.photos/200/300",
-          }}
-        />
-      </TouchableHighlight>
+      <Button
+        title={"Click me!"}
+        onPress={() => {
+          Alert.alert("customized alert", "Alert message", [
+            {
+              text: "yes",
+              onPress: () => console.log("Yes button was pressed"),
+            },
+            {
+              text: "no",
+              onPress: () => console.log("No button was pressed"),
+            },
+          ]);
+        }}
+      />
     </View>
   );
 }
