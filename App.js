d27fc244e47aafeea02d18beea08c87f06c58905
ihab-sensei <ib-mashhadani@hotmail.com>
2021-02-06 14:47:09 +0300

    style borders

--- App.js
+++ App.js
@@ -1,6 +1,27 @@
 import React from "react";
+import { View } from "react-native";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  return <ViewImageScreen />;
+  return (
+    <View
+      style={{
+        backgroundColor: "#fff",
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <View
+        style={{
+          width: 100,
+          height: 100,
+          backgroundColor: "dodgerblue",
+          borderWidth: 10,
+          borderColor: "royalblue",
+          borderRadius: 50,
+        }}
+      ></View>
+    </View>
+  );
 }
