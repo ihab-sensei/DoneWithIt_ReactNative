87129ee2d70e448f77eddf4d73777d043384d747
ihab-sensei <ib-mashhadani@hotmail.com>
2021-02-13 23:33:05 +0300

    buliding listing details screen

--- App.js
+++ App.js
@@ -3,22 +3,8 @@ import { View } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import Card from "./app/components/Card";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 
 export default function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: "#f8f4f4",
-        padding: 20,
-        paddingTop: 100,
-      }}
-    >
-      <Card
-        title="Red jacket for sale"
-        subTitle="100$"
-        image={require("./app/assets/jacket.jpg")}
-      />
-    </View>
-  );
+  return <ListingDetailsScreen />;
 }
---
+++ app/screens/ListingDetailsScreen.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Image, View, StyleSheet } from "react-native";
+
+import AppText from "../components/AppText";
+import colors from "../config/colors";
+
+function ListingDetailsScreen(props) {
+  return (
+    <View>
+      <Image style={styles.image} source={require("../assets/jacket.jpg")} />
+      <View style={styles.detailsContainer}>
+        <AppText style={styles.title}>Red jacket for sale</AppText>
+        <AppText style={styles.price}>100$</AppText>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  detailsContainer: {
+    padding: 20,
+  },
+  image: {
+    width: "100%",
+    height: 300,
+  },
+  price: {
+    fontWeight: "bold",
+    fontSize: 20,
+    color: colors.secondary,
+    marginVertical: 10,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+});
+
+export default ListingDetailsScreen;
