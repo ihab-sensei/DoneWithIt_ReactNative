f3cabf26181027a0cb8c03cedfe08f60a6fdf4cd
ihab-sensei <ib-mashhadani@hotmail.com>
2021-02-19 23:23:05 +0300

    Add swipe gesture and ListItemDeleteAction

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -2,6 +2,7 @@ import React from "react";
 import { FlatList, StyleSheet } from "react-native";
 
 import ListItem from "../components/ListItem";
+import ListItemDeleteAction from "../components/ListItemDeleteAction";
 import ListItemSeperator from "../components/ListItemSeperator";
 import Screen from "../components/Screen";
 
@@ -32,6 +33,7 @@ function MessagesScreen(props) {
             subTitle={item.description}
             image={item.image}
             onPress={() => console.log("message selected", item)}
+            renderRightActions={ListItemDeleteAction}
           />
         )}
         ItemSeparatorComponent={() => <ListItemSeperator />}
