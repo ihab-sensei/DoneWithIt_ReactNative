2cd9b1e1e163bc082446475bd32ad50feaf52031
ihab-sensei <ib-mashhadani@hotmail.com>
2021-02-20 20:41:42 +0300

    extending ListItem component

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
 import React from "react";
 import Icon from "./app/components/Icon";
+import ListItem from "./app/components/ListItem";
 import Screen from "./app/components/Screen";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 export default function App() {
   return (
     <Screen>
-      <Icon
-        name="email"
-        size={50}
-        backgroundColor="red"
-        iconColor="white"
-      ></Icon>
+      <ListItem
+        title="My title"
+        ImageComponent={<Icon name="email" />}
+      ></ListItem>
     </Screen>
   );
 }
