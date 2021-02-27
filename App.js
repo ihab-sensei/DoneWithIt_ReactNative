9174d41b05315f5747991b8e838da6c2cd96f505
ihab-sensei <ib-mashhadani@hotmail.com>
2021-02-27 18:23:06 +0300

    build login screen

--- App.js
+++ App.js
@@ -1,27 +1,6 @@
-import React, { useState } from "react";
-import { Switch } from "react-native";
-import AppPicker from "./app/components/AppPicker";
-
-import Screen from "./app/components/Screen";
-
-const categories = [
-  { label: "Furniture", value: 1 },
-  { label: "Clothing", value: 2 },
-  { label: "Cameras", value: 3 },
-];
+import React from "react";
+import LoginScreen from "./app/screens/LoginScreen";
 
 export default function App() {
-  const [category, setCategory] = useState();
-
-  return (
-    <Screen>
-      <AppPicker
-        selectedItem={category}
-        onSelectItem={(item) => setCategory(item)}
-        items={categories}
-        icon="apps"
-        placeholder="Category"
-      />
-    </Screen>
-  );
+  return <LoginScreen />;
 }
---
+++ app/screens/LoginScreen.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { Image, StyleSheet } from "react-native";
+
+import Screen from "../components/Screen";
+import AppTextInput from "../components/AppTextInput";
+import AppButton from "../components/AppButton";
+
+function LoginScreen(props) {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  return (
+    <Screen style={styles.container}>
+      <Image style={styles.logo} source={require("../assets/logo-red.png")} />
+      <AppTextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        icon="email"
+        placeholder="Email"
+        keyboardType="email-address"
+        textContentType="emailAddress"
+        onChangeText={(text) => setEmail(text)}
+      />
+      <AppTextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        icon="lock"
+        placeholder="Password"
+        textContentType="password"
+        secureTextEntry
+        onChangeText={(text) => setPassword(text)}
+      />
+      <AppButton title="Login" onPress={() => console.log(email, password)} />
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({
+  logo: {
+    width: 100,
+    height: 100,
+    alignSelf: "center",
+    marginVertical: 50,
+  },
+  container: {
+    padding: 10,
+  },
+});
+
+export default LoginScreen;
