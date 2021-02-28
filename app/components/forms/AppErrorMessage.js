ad0348af671549aa62720fafab2f82a37e303076
ihab-sensei <ib-mashhadani@hotmail.com>
2021-02-28 15:42:24 +0300

    move form related components in their distinctive folder

--- app/components/forms/AppErrorMessage.js
+++ app/components/forms/AppErrorMessage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { StyleSheet } from "react-native";
-import AppText from "./AppText";
+import AppText from "../AppText";
 
 function AppErrorMessage({ error, visible }) {
   if (visible && error) return <AppText style={styles.error}>{error}</AppText>;
